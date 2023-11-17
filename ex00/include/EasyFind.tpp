4759497107b96539a064641831c58dc155654332
Iurii Ovsiannikov <jgoldste@z2t17c3.42bangkok.com>
2023-11-17 19:45:36 +0700

    ex00_Nov17_19-45

--- ex00/include/EasyFind.tpp
+++ ex00/include/EasyFind.tpp
@@ -6,18 +6,20 @@
 /*   By: jgoldste <jgoldste@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 12:19:26 by jgoldste          #+#    #+#             */
-/*   Updated: 2023/11/16 19:43:47 by jgoldste         ###   ########.fr       */
+/*   Updated: 2023/11/17 19:10:24 by jgoldste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_TPP
 #define EASYFIND_TPP
 
+#include <stdexcept>
 #include "EasyFind.hpp"
 
-struct my_value_not_found : std::exception {
-	const char* what() const throw {
-		return "The value not found";
+class value_not_found : public std::exception {
+	public:
+		const char* what() const throw() {
+			return "The value not found";
 	}
 };
 
@@ -25,7 +27,7 @@ template <typename T>
 typename T::iterator easyFind(T& container, int value) {
 	typename T::iterator it = std::find(container.begin(), container.end(), value);
 	if (it == container.end())
-		throw my_value_not_found();
+		throw value_not_found();
 	return it;
 }
 
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,61 @@
 /*   By: jgoldste <jgoldste@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 12:18:49 by jgoldste          #+#    #+#             */
-/*   Updated: 2023/11/13 12:18:51 by jgoldste         ###   ########.fr       */
+/*   Updated: 2023/11/17 19:44:54 by jgoldste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+#include <deque>
+#include "EasyFind.hpp"
+
+#define SIZE 6
+
+int main() {
+	int value;
+
+	std::srand(std::time(0));
+	value = std::rand() % 7;
+	std::cout << "Generated value to find: " << value << std::endl;
+	std::vector<int> myVector(SIZE);
+	for (int i = 0; i <= SIZE; i++) {
+		try {
+			myVector.at(i) = i;
+		} catch (const std::out_of_range& e) {}
+	}
+	try {
+		for (std::vector<int>::iterator it = myVector.begin(); it != myVector.end(); it++)
+			std::cout << "Vector<int> value: " << *it << std::endl;
+		std::vector<int>::iterator it = easyFind(myVector, value);
+		std::cout << "Value found is: " << *it << std::endl;
+	} catch (const value_not_found& e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	std::cout << std::endl;
+
+	std::srand(std::time(0));
+	value = std::rand() % 7;
+	std::cout << "Generated value to find: " << value << std::endl;
+	std::deque<int> myDeque;
+	for (int i = 0; i < SIZE; i += 2) {
+		myDeque.push_front(i);
+		myDeque.push_back(i + 1);
+	}
+	try {
+		for (std::deque<int>::iterator it = myDeque.begin(); it != myDeque.end(); it++)
+			std::cout << "Deque<int> value: " << *it << std::endl;
+		std::deque<int>::iterator it = easyFind(myDeque, value);
+		std::cout << "Value found is: " << *it << std::endl;
+	} catch (const value_not_found& e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	return 0;
+}
