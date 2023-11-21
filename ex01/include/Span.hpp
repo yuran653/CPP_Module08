4cce9690851f7845fb09eccd5ca37ba6af972d98
Iurii Ovsiannikov <jgoldste@z2t9c1.42bangkok.com>
2023-11-21 19:42:30 +0700

    ex01_Nov21_19-42

--- ex01/include/Span.hpp
+++ ex01/include/Span.hpp
@@ -6,7 +6,21 @@
 /*   By: jgoldste <jgoldste@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 12:21:07 by jgoldste          #+#    #+#             */
-/*   Updated: 2023/11/13 12:21:08 by jgoldste         ###   ########.fr       */
+/*   Updated: 2023/11/21 19:34:21 by jgoldste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+class Span {
+	private:
+	
+
+	public:
+		unsigned int	shortestSpan();
+		unsigned int	longestSpan();
+
+};
+
+#endif
